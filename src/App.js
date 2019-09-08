a041f9046f5b4927863bc017878728f61e510e9d
Stephen Woosley <stephenrwoosley@gmail.com>
2019-09-08 18:11:15 -0400

    class component transformation, move to sass & carbon npm style imports, content data working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React from 'react';
-import { Header, HeaderName } from "carbon-components-react";
+import { Breadcrumb, BreadcrumbItem, Header, HeaderName } from "carbon-components-react";
+import { Content } from 'carbon-components-react/lib/components/UIShell';
 import logo from "./images/stanger-things_raw.png";
-import './App.css';
+import en_US from "./data/en_US";
+import la_PG from "./data/la_PG";
+import './App.scss';
 
-function App() {
-  return (
-    <div>
-     <Header aria-label="Stranger-Things-Logo">
+
+class App extends React.Component {
+
+  // toggle switch will change language
+  // it will flip a boolean languageChoice that is in state
+  // componentDidMount -> based on languageChoice, set contentData state to either PG or EN
+  state = {
+    languageChoice: "en_US",
+    contentData: []
+  }
+
+  componentDidMount() {
+    this.state.languageChoice === "en_US" 
+      ? this.setState({contentData: { ...en_US}})
+      : this.setState({contentData: { ...la_PG}});
+  }
+
+  render() {
+    return (
+      <div>
+      <Header aria-label="Stranger-Things-Logo">
         <HeaderName href="#" prefix="">
           <img src={logo} alt="stranger-things-logo" className="logo"/>
         </HeaderName>
       </Header>
+      <Content>
+        <div className="bx--grid">
+          <div className="bx--row landing-page__banner">
+            <div className="bx--col-lg-12">
+              {/* <Breadcrumb noTrailingSlash>
+                <BreadcrumbItem>
+                  <a href="/">Getting started</a>
+                </BreadcrumbItem>
+              </Breadcrumb> */}
+              <h1 className="landing-page__heading .bx--type-semibold">
+                {this.state.contentData.heading}
+              </h1>
+            </div>
+          </div>
+        </div>
+      </Content>
     </div>
-  );
+    );
+  }
 }
 
 export default App;

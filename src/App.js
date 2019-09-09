72b4e29a7daf67f0c62af97fae85873a6b0f99c2
Stephen Woosley <stephenrwoosley@gmail.com>
2019-09-09 17:28:10 -0400

    completed project - navbar and language choice working, media added, season info added, did you know content and section headings

--- src/App.js
+++ src/App.js
@@ -1,48 +1,198 @@
 import React from 'react';
-import { Breadcrumb, BreadcrumbItem, Header, HeaderName } from "carbon-components-react";
-import { Content } from 'carbon-components-react/lib/components/UIShell';
-import logo from "./images/stanger-things_raw.png";
-import en_US from "./data/en_US";
-import la_PG from "./data/la_PG";
+import { 
+  Accordion, 
+  AccordionItem, 
+  ContentSwitcher, 
+  Switch, 
+  ToggleSmall 
+} from 'carbon-components-react';
+import { 
+  Content,
+  Header,
+  HeaderMenuButton,
+  HeaderMenuItem,
+  HeaderName,
+  HeaderNavigation,
+} from 'carbon-components-react/lib/components/UIShell';
+import HeaderContainer from "carbon-components-react/lib/components/UIShell/HeaderContainer";
+import { Carousel } from 'react-responsive-carousel';
+import { Player } from 'video-react';
+import logo from './images/stanger-things_raw.png';
+import en_US from './data/en_US';
+import la_PG from './data/la_PG';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './App.scss';
 
 
 class App extends React.Component {
-
-  // toggle switch will change language
-  // it will flip a boolean languageChoice that is in state
-  // componentDidMount -> based on languageChoice, set contentData state to either PG or EN
   state = {
-    languageChoice: "en_US",
-    contentData: []
+    languageChoice: 'en_US',
+    contentData: en_US,
+    image: en_US.gallery[0].src,
+    currentSeason: 1,
+    videoURL: en_US["video-embed"],
+    currentSnippet: en_US.snippets[2],
+    currentLocation: en_US.locations[4]
   }
 
   componentDidMount() {
-    this.state.languageChoice === "en_US" 
-      ? this.setState({contentData: { ...en_US}})
-      : this.setState({contentData: { ...la_PG}});
+    this.timedSwitcher(this.state.contentData.snippets, 10000);
+    this.timedSwitcher(this.state.contentData.locations, 6350);
+  }
+
+  toggleLanguage = () => {
+    if (this.state.languageChoice === 'en_US') {
+      this.setState({languageChoice: 'la_PG'});
+      this.setState({contentData: la_PG});
+    }
+    else if (this.state.languageChoice === 'la_PG') {
+      this.setState({languageChoice: 'en_US'});
+      this.setState({contentData: en_US});
+    }
+  }
+
+  switchSeasons = (num) => {
+    this.setState({currentSeason: num})
+  }
+
+  timedSwitcher = (items, time) => {
+    let count = 0;
+    let showSnippet = setInterval(() => {
+      if(items === this.state.contentData.snippets) {
+        this.setState({currentSnippet:items[count]});
+      }
+      else if (items === this.state.contentData.locations) {
+        this.setState({currentLocation:items[count]});
+      }
+      count++;
+      if (count === items.length) {
+        count = 0;
+      }
+    }, time);
   }
 
   render() {
+
+    const toggleProps = () => ({
+      labelText: 'Page Language',
+      labelA: 'PG',
+      labelB: 'EN',
+      disabled: false
+    });
+
     return (
-      <div>
-      <Header aria-label="Stranger-Things-Logo">
-        <HeaderName href="#" prefix="">
-          <img src={logo} alt="stranger-things-logo" className="logo"/>
-        </HeaderName>
-      </Header>
+    <div className="container">
+          <Header aria-label="IBM Platform Name">
+            <HeaderMenuButton
+              aria-label="Open menu"
+              // onClick={onClickSideNavExpand}
+              isActive={false}
+            />
+            <HeaderName prefix=''>
+              <img src={logo} alt='stranger-things-logo' className='logo'/>
+              <span className='titleSpan'>in RTP</span>
+            </HeaderName>
+            <HeaderNavigation aria-label='navigation options'>
+              <HeaderMenuItem href="#didYouKnow">Did You Know?</HeaderMenuItem>
+              <HeaderMenuItem href="#topEpisodes">Top Episodes</HeaderMenuItem>
+              <HeaderMenuItem href="#media">Media</HeaderMenuItem>
+            </HeaderNavigation>
+            <ToggleSmall
+              defaultToggled
+              {...toggleProps()}
+              className='toggle-lang'
+              id='toggle-1'
+              aria-label='language toggle'
+              onChange={this.toggleLanguage}
+            /> 
+        </Header>
       <Content>
-        <div className="bx--grid">
-          <div className="bx--row landing-page__banner">
-            <div className="bx--col-lg-12">
-              {/* <Breadcrumb noTrailingSlash>
-                <BreadcrumbItem>
-                  <a href="/">Getting started</a>
-                </BreadcrumbItem>
-              </Breadcrumb> */}
-              <h1 className="landing-page__heading .bx--type-semibold">
+        <div className='bx--grid'>
+          <div className='bx--row section-divider'>
+            <div className='bx--col-lg-2'>
+            </div>
+            <div className='bx--col-lg-8'>
+              <h1 className='landing-page__heading'>
                 {this.state.contentData.heading}
               </h1>
+            </div>
+            <div className='bx--col-lg-2'>
+
+            </div>
+          </div>
+          <div className="bx--row section-divider">
+            <div className='bx--col-lg-2'></div>
+            <div className='bx--col-lg-8'>
+              <h3 className='page-description'>{this.state.contentData.description}</h3>
+            </div>
+            <div className='bx--col-lg-2'></div>
+          </div>
+          <div className='bx--row section-divider' id="didYouKnow">
+            <div className='bx--col-lg-2'></div>
+              <div className='bx--col-lg-8'>
+                <h1 className='section-heading'>
+                <span role="img" aria-label="thinking-icon">🤔</span> Did You Know? <span role="img" aria-label="thinking-icon">🤔</span>
+                </h1>
+              </div>
+            <div className='bx--col-lg-2'></div>
+            <div className='bx--col-lg-6'>
+              <h3 className="section-subheading">Show Information & Quotes</h3>
+              <h4 className="snippet-holder">{this.state.currentSnippet}</h4>
+            </div>
+            <div className='bx--col-lg-6'>
+              <h3 className="section-subheading">Durham Locations Referenced</h3>
+              <h3 className="snippet-holder">{this.state.currentLocation}</h3>
+            </div>
+          </div>
+          <div className='bx--row section-divider' id="topEpisodes">
+            <div className='bx--col-lg-2'></div>
+              <div className='bx--col-lg-8'>
+                <h1 className='section-heading'>
+                <span role="img" aria-label="trophy-icon">🏆</span> Top Stranger Things Episodes <span role="img" aria-label="trophy-icon">🏆</span>
+                </h1>
+              </div>
+            <div className='bx--col-lg-2'></div>
+            <div className='bx--col-lg-12'>
+              <ContentSwitcher>
+                  <Switch name='one' text='Season 1' onClick={() => this.switchSeasons(1)} />
+                  <Switch name='two' text='Season 2' onClick={() => this.switchSeasons(2)} />
+              </ContentSwitcher>
+              <Accordion>
+                <Season 
+                  content={this.state.contentData}
+                  seasonNumber={this.state.currentSeason}
+                />
+              </Accordion>
+            </div>
+          </div>
+          <div className='bx--row section-divider' id="media">
+            <div className='bx--col-lg-2'></div>
+              <div className='bx--col-lg-8'>
+                <h1 className='section-heading'>
+                <span role="img" aria-label="media-icon">📽</span> Stranger Things & Durham Media <span role="img" aria-label="media-icon">📽</span>
+                </h1>
+              </div>
+            <div className='bx--col-lg-2'></div>
+            <div className='bx--col-lg-6'>
+              <Player>
+                <source src={this.state.videoURL} />
+              </Player>
+            </div>
+            <div className='bx--col-lg-6 image-slider'>
+              <Carousel showIndicators={false}>
+                  <div>
+                      <img src={this.state.image} alt='slider' />
+                      <p className='legend'>Legend 1</p>
+                  </div>
+                  <div>
+                      <img src={this.state.image} alt='slider' />
+                      <p className='legend'>Legend 2</p>
+                  </div>
+                  <div>
+                      <img src={this.state.image} alt='slider' />
+                      <p className='legend'>Legend 3</p>
+                  </div>
+              </Carousel>
             </div>
           </div>
         </div>
@@ -50,6 +200,23 @@ class App extends React.Component {
     </div>
     );
   }
+}
+
+function Season(props) {
+  let episodes = [props.content['episode-list']];
+  episodes = props.content['episode-list'].filter((episode) => episode.season === props.seasonNumber);
+  const returnedEpisodes = episodes.map((episode, index) => {
+    return (
+      <AccordionItem 
+        key={index}
+        className='season-episode-data'
+        title={episode.name}
+      >
+        <h5>Rating: {episode.rating}</h5>
+      </AccordionItem>
+    );
+  });
+  return returnedEpisodes;
 }
 
 export default App;
